24361c8fffb7b3fe1999492707d3401d54773791
Spiridon Madalina <78969330+Madalina0303@users.noreply.github.com>
2022-10-26 09:04:43 +0300

    Add files via upload

---
+++ twitter.py
@@ -0,0 +1,24 @@
+
+
+# FACADE PATTERN
+class TwitterClawler():
+    def __init__(self):
+        self.twitter_api = None
+        self.data = None
+        self.api_key = None
+        self.filters = None
+    def authenticate(self, auth_file_path=None):
+        1 == 1
+
+    def request_data(self, tag, start_time, end_time, location):
+        1 == 1
+
+    def filter_data(self):
+        1 == 1
+
+    def export_data_as_json(self, path):
+        1 == 1
+
+    #OBSERVER
+    def monitor_for_new_posts(self):
+        1 == 1
