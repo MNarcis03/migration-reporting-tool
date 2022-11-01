79ef70a92d381b9e04b0f6d31e6efd7d7083ac12
Vultur Iulian <iulyusdiaz@yahoo.com>
2022-11-02 01:48:24 +0200

    fixed pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,59 +3,16 @@ import React from 'react';
 import { BrowserRouter, createBrowserRouter, Navigate, Route, RouterProvider, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
 import Header from './components/header/Header';
+import MapPage from './components/map/MapPage';
+import StatisticsPage from './components/statistics/StatisticsPage';
 import Statistics from './components/statistics/StatisticsPage';
 import { COLOR_BACKGROUND, COLOR_ON_SURFACE, COLOR_ON_SURFACE_VARIANT, COLOR_PRIMARY } from './styling/colors';
 
-const MapPage = React.lazy(() => import('./components/map/MapPage'));
-const StatisticsPage = React.lazy(() => import('./components/statistics/StatisticsPage'));
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <p>Ana are mere</p>,
-        errorElement: <p>error</p>
-    },
-    {
-        path: "/map",
-        element: <MapPage />,
-    },
-    {
-        path: "/statistics",
-        element: <Statistics />,
-    },
-]);
+// const MapPage = React.lazy(() => import('./components/map/MapPage'));
+// const StatisticsPage = React.lazy(() => import('./components/statistics/StatisticsPage'));
 
 const theme = createTheme({
-    // components: {
-    //     MuiDatePicker: {
-    //         styleOverrides: {
-    //             root: {
-    //                 color: '#ff0000',
-    //                 primary: {
-    //                     // light: '#ff0000',
-    //                     main: '#ffffff',
-    //                     // dark: '#000000'
-    //                 },
-    //                 backgroundColor: 'red',
-
-
-    //             },
-    //         },
-    //     },
-    // },
     palette: {
-        // primary: {
-        //     light: '#757ce8',
-        //     main: '#3f50b5',
-        //     dark: '#002884',
-        //     contrastText: '#fff',
-        //   },
-        //   secondary: {
-        //     light: '#ff7961',
-        //     main: '#f44336',
-        //     dark: '#ba000d',
-        //     contrastText: '#fff',
-        //   },
         primary: {
             // light: '#ff0000',
             main: COLOR_PRIMARY,
@@ -79,9 +36,7 @@ function App() {
                     <Routes>
                         <Route path="/statistics" element={<StatisticsPage />} />
                         <Route path="/map" element={<MapPage />} />
-                        <Route path="*" element={<Navigate to={'/map'} />}>
-                            
-                        </Route>
+                        <Route path="*" element={<Navigate to={'/map'} />} />
                     </Routes>
                 </div>
             </BrowserRouter>
--- frontend/src/components/statistics/StatisticsPage.tsx
+++ frontend/src/components/statistics/StatisticsPage.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
 import styles from './StatisticsPage.module.scss';
 
-interface StatisticsProps {}
+interface StatisticsProps { }
 
 const Statistics: FC<StatisticsProps> = () => (
-  <div className={styles.Statistics}>
-    Statistics Component
-  </div>
+    <div className={styles.Statistics}>
+        Statistics Component
+    </div>
 );
 
 export default Statistics;
